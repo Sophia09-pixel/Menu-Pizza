e4a0ee1cc7ca8bc0944a03c45ee02deee565da8e
Sophia <sophia.dev0903@gmail.com>
2025-04-08 20:49:17 -0300

    fix: ajustes no card

--- lib/menu.dart
+++ lib/menu.dart
@@ -6,6 +6,20 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MenuCard();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pizza Menu"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/menu_item.dart
+++ lib/menu_item.dart
@@ -1,64 +1,55 @@
 import 'package:flutter/material.dart';
 
-class MenuCard extends StatelessWidget {
-  const MenuCard({super.key});
+class MenuItem extends StatelessWidget {
+  const MenuItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Pizza Menu"),
-      ),
-      body: Column(
-        children: [
-          Card(
-            elevation: 2, // sombra do card
-            child: Padding(
-              padding: const EdgeInsets.all(12.0), // espacamento dentro do card
-              child: Row(
+    return Card(
+      elevation: 2, // sombra do card
+      child: Padding(
+        padding: const EdgeInsets.all(12.0), // espacamento dentro do card
+        child: Row(
+          children: [
+            Image.asset(
+              "images/margherita.jpg",
+              width: 100,
+              height: 100,
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              // para o texto pegar o espaço completo do componente
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(
-                    "images/margherita.jpg",
-                    width: 100,
-                    height: 100,
+                  Text(
+                    "Pizza Margherita",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  SizedBox(
-                    width: 16,
+                  Text(
+                    "Tomato and Muzarella",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black87,
+                    ),
                   ),
-                  Expanded(
-                    // para o texto pegar o espaço completo do componente
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Pizza Margherita",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "Tomato and Muzarella",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        Text(
-                          "10",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black54,
-                          ),
-                        ),
-                      ],
+                  Text(
+                    "10",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
